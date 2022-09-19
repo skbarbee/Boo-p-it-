0f4966610e44ea62316af0489ffed958c8b083de
skbarbee <barbee.s.k@gmail.com>
2022-09-19 16:53:19 -0400

    added pattern follow, updated ID's

--- java/index.js
+++ java/index.js
@@ -4,29 +4,70 @@
 // if they follow pattern, increase pattern by 1
 //if they don't game'over
 
-const bopIt = document.querySelector("#boo-p")
-const twistIt = document.querySelector("#ghost")
-const pullIt = document.querySelector("#spider")
+const bopIt = document.querySelector("#Bop-It")
+const twistIt = document.querySelector("#Twist-It")
+const pullIt = document.querySelector("#Pull-It")
 const readOut = document.querySelector(".read-out")
+const gameBoard = document.querySelector(".game-container")
 // console.log(bopIt,  twistIt, pullIt)
 
 
-bopIt.addEventListener("click", (e)=>{
-    console.log("bop it was clicked")
+document.addEventListener("DOMContentLoaded", ()=>{
+    console.log("bop it was loaded")
     bopIt.classList.add("bounce-top")
-})
-
-twistIt.addEventListener("click", (e)=>{
+    twistIt.classList.add('rotate-center')
     console.log("twist it it was clicked") 
-    let classList = twistIt.classList.add('rotate-center')
-    
-    
-})
-
-pullIt.addEventListener("click", (e)=>{
     console.log("pull it was clicked")
     pullIt.classList.add("slide-bottom")
-})
+    patternFunction();
 
-const twistFunction = (e) => {
-    twistIt.classList.add("rotate-center")}
+    })
+
+
+
+
+// make empty arrays for results to go it 
+let computerTurn = []
+
+let playerTurn = []
+
+//function that will randomize between 1,2,3 
+// each level must go up
+let level = 0
+
+const choices = ["Bop-It", "Twist-It", "Pull-It"]
+//     let randomChoices = choices[Math.floor(Math.random()* choices.length)]
+const patternFunction = ()=> {
+    // const choices = ["Bop-It", "Twist-It", "Pull-It"]
+    let randomChoices = choices[Math.floor(Math.random()* choices.length)]
+    computerTurn.push(randomChoices)
+    readOut.innerText = (computerTurn)
+     
+}
+
+const gamePlay = ()=>{
+    
+    // add eventlistener to see if player copied
+gameBoard.addEventListener("click", (e)=>{
+    // add click to player array
+        //    console.log(e.target.id)
+           playerTurn.push(e.target.id)
+            console.log(playerTurn)
+            console.log(computerTurn)
+        //if player follers the pattern make a new pattern
+            if(playerTurn[0] == computerTurn[0]){
+                playerTurn = []
+                computerTurn = []
+                let newRandomChoices = choices[Math.floor(Math.random()* choices.length)]
+                computerTurn.push(newRandomChoices)
+                readOut.innerText = (computerTurn)
+                
+                
+                
+            }else{
+        readOut.innerText = "DOOOH!"
+    }
+})
+}
+
+gamePlay()
