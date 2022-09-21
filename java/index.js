36f841668431e8b175060233e11828622976bdc2
skbarbee <barbee.s.k@gmail.com>
2022-09-21 13:45:41 -0400

    timing issue resolved

--- java/index.js
+++ java/index.js
@@ -9,8 +9,14 @@ const twistIt = document.querySelector("#Twist-It")
 const pullIt = document.querySelector("#Pull-It")
 const readOut = document.querySelector(".read-out")
 const gameBoard = document.querySelector(".game-container")
-// console.log(bopIt,  twistIt, pullIt)
+const startButton = document.querySelector("#start")
 
+
+let level = 0
+let playerTurn = []
+let computerTurn = []
+const choices = ["Bop-It","Twist-It","Pull-It"]
+let randomChoice = choices[Math.floor((Math.random())* (choices.length))]
 
 document.addEventListener("DOMContentLoaded", ()=>{
     console.log("bop it was loaded")
@@ -20,75 +26,66 @@ document.addEventListener("DOMContentLoaded", ()=>{
     
     pullIt.classList.add("slide-bottom")
     console.log("pull it was loaded")
-    patternFunction();
-
-    })
-
-
-
-
-// make empty arrays for results to go it 
-let computerTurn = []
-
-let playerTurn = []
-
-//function that will randomize between 1,2,3 
-// each level must go up
-let level = 0
-
-const choices = ["Bop-It", "Twist-It", "Pull-It"]
-//     let randomChoices = choices[Math.floor(Math.random()* choices.length)]
-const patternFunction = ()=> {
-    // const choices = ["Bop-It", "Twist-It", "Pull-It"]
-    let randomChoices = choices[Math.floor(Math.random()* choices.length)]
-    computerTurn.push(randomChoices)
-    readOut.innerText = (computerTurn)
-     
-}
-
-const gamePlay = ()=>{
     
-    // add eventlistener to see if player copied
-    gameBoard.addEventListener("click", (e)=>{
-    // add click to player array
-        //    console.log(e.target.id)
-           playerTurn.push(e.target.id)
-            console.log("Player:",playerTurn)
-            console.log("Computer:", computerTurn)
-              checkPattern()     
-
-    // if(checkPattern = "false"){
-    //     clearInterval(checkPattern)
-    // }
 })
-}
-//if player follows the pattern make a new pattern
-        //need to make a time limit to follow 
-const checkPattern = () =>{
-            if(playerTurn[0] == computerTurn[0]){
-                level += 1
-                playerTurn = []
-                computerTurn = []
-                let newRandomChoices = choices[Math.floor(Math.random()* choices.length)]
-                computerTurn.push(newRandomChoices)
-                readOut.innerText = (computerTurn)
-                // setInterval(patternFunction,1500)
-            }else{
-                
-                readOut.innerText = `DOOOH!  You got to level ${level}`
-                console.log("you got to level", level)
-                // clearInterval(patternFunction)
-                gameBoard.removeEventListener("click",()=>{
-                //    return false
-                })
-                }
+
+
+
+
+const startGame = ()=>{
+// board has to be empty
+
+    gameBoard.style.visibility='visible'
+    playerTurn = []
+    computerTurn = []
+    level = 0
+    readOut.style.color = "#bada55"
+    readOut.innerText = "ready..."
+//call the first direction give player a chance to get ready
+    setTimeout(()=>{
+        readOut.innerText = randomChoice
+        computerTurn.push(randomChoice)
+    }, 1000)
+//timeout set because interval was going too fast
+    
+    gameBoard.addEventListener("click", (e)=>{
+   // add's player click to array 
+        playerTurn.push(e.target.id)
+        
+            const intervalID = setInterval(()=>{
             
+                if(playerTurn[0]=== computerTurn[0]){
+                // clear out arrays
+                    playerTurn =[]
+                    computerTurn=[]
+                // add level
+                    level += 1
+                 // set new choice 
+                    let randomChoice = choices[Math.floor((Math.random())* (choices.length))]
+                    // console.log(`it's level ${level}`,playerTurn, computerTurn, computerTurn[0] === playerTurn[0])
+                    readOut.innerText= `...${randomChoice}`
+                    computerTurn.push(randomChoice)
+                
+                //change text color incase random choice is the same choice
+                    if (level % 2 !== 0){
+                        readOut.style.color = "black"
+                    } else {
+                        readOut.style.color = "purple"
+                    }
+                }else{
+                    console.log("game lost")
+                    clearInterval(intervalID)
+                    gameBoard.style.visibility ="hidden" 
+                    readOut.innerText = `you got to level ${level}`} 
+         
+            },3000)
+            
+        
+     })
+    
 }
-const stopInterval = (pattern)=>{
-    clearInterval(pattern)
-}
 
 
+   
 
-
-gamePlay()
+startButton.addEventListener("click", startGame)
